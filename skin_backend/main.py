b123bf408ba8cfa60f06b1adea8d36ef6a443511
Laiba <laibatahir6002@gmail.com>
2026-08-20 12:20:28 +0500

    Fix main startup

--- skin_backend/main.py
+++ skin_backend/main.py
@@ -291,12 +291,3 @@ def chat(request: ChatRequest):
         "response": response
     }
 
-if __name__ == "__main__":
-    import uvicorn
-    import os
-
-    uvicorn.run(
-        app,
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 8000))
-    )
